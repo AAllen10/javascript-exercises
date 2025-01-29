10f7ea79139b47d363fcad1c88976aa47d5d8ffd
AAllen10 <168362187+AAllen10@users.noreply.github.com>
2025-01-29 22:22:36 +0000

    Finish exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
 
+const repeatString = function(string, num) {
+    newString = '';
+    if(num<0){
+        return 'ERROR';
+    }
+    if(num ===0){return '';}
+    if(string === '' || num === 1){return string;}
+
+    for(i = 0; i < num; i++){
+        newString+= string;
+    }
+    return newString;
 };
 
 // Do not edit below this line
