6b0f406fca293786289f31c64d892e0aa5660ca9
AAllen10 <168362187+AAllen10@users.noreply.github.com>
2025-01-29 22:28:16 +0000

    Finish exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,9 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    revString ="";
+    for(i=String(string).length - 1; i >= 0; i--){
+        revString += String(string)[i];
+    }
+    return revString;
 
 };
 
