e96637247f27010ca89865c884bcbeadf9cf12a8
AAllen10 <168362187+AAllen10@users.noreply.github.com>
2025-01-30 02:25:05 +0000

    Finish exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...values) {
+    
+    for(const value of values){
+        for(i = 0; i < array.length; i++){
+            let foundIndex = array.indexOf(value);
+            if(foundIndex > -1){
+                array.splice(foundIndex, 1);
+                i--;
+            }                
+            
+        }
+    }
+
+    return array;
+
 };
 
 // Do not edit below this line
