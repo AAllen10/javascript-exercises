ce3417730e7c0a571fba373eb20f063b4f703716
AAllen10 <168362187+AAllen10@users.noreply.github.com>
2025-01-30 03:20:40 +0000

    Finish exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(value) {
+  let celValue = (value - 32) * 5/9;
+  celValue = Number(celValue.toFixed(1));
+  return celValue;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(value) {
+  let farValue = (value * 9/5) + 32;
+  farValue = Number(farValue.toFixed(1));
+  return farValue;
 };
 
 // Do not edit below this line
