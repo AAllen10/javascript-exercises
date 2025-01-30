4d30a94bdfc3916a18b850263cf0f5ae79e40d14
AAllen10 <168362187+AAllen10@users.noreply.github.com>
2025-01-30 02:50:17 +0000

    Finish exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,18 @@
-const sumAll = function() {
+const sumAll = function(startVal, endVal) {
+    let biggerNum = 0;
+    let smallerNum = 0;
+    let total = 0;
+    
+    if(startVal > endVal){biggerNum = startVal; smallerNum = endVal}else{biggerNum = endVal; smallerNum = startVal;}
+    if(startVal < 0 || endVal < 0){return 'ERROR';}
+    if(startVal % 1 != 0 || endVal % 1 != 0){return 'ERROR';}
+    if(typeof(startVal)!= "number" || typeof(endVal) != "number"){return 'ERROR';}
+    
+    for(i=smallerNum; i<=biggerNum;i++){
+        total+=i;
+    }
+
+    return total;
 
 };
 
