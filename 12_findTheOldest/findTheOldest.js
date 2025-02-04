3d0762d89a0878c61f3fd2fd79799449d05fdb57
AAllen10 <168362187+AAllen10@users.noreply.github.com>
2025-02-04 16:14:03 +0000

    Finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(peopleArr) {
+    let currentOldest = peopleArr[0];
+    peopleArr.forEach(element => {
+        if("yearOfDeath" in element){
+           let age = element.yearOfDeath - element.yearOfBirth;
+           element.age = age;
+        }
+        else{
+            let age = (new Date().getFullYear() - element.yearOfBirth); //update
+            element.age = age;
+        }
+        if(element.age > currentOldest.age){
+            currentOldest = element;
+        }
+    });
+    return currentOldest;
 };
 
 // Do not edit below this line
