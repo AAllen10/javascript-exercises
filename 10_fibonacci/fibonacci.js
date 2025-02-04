6e2bbe22489732ae0db4c54d7feda75016b91992
AAllen10 <168362187+AAllen10@users.noreply.github.com>
2025-02-04 04:21:18 +0000

    Finish exercice 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  if(typeof num !== "number"){
+        actualNum = parseInt(num);
+  }
+  else{ actualNum = num}
+  if(actualNum < 0){return "OOPS";};
+  if(actualNum === 0){return 0;};
+  if(actualNum === 1){return 1;}; //messy, will look at solution after
+  let count = 0;
+  let twoLess =0;
+  let oneLess =1;
+  for(i = 2; i <= actualNum; i++){
+    count = oneLess + twoLess;
+    twoLess = oneLess;
+    oneLess = count;
+  }
+  return count;
+  
 };
 
 // Do not edit below this line
