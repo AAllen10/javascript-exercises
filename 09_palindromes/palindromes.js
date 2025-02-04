6701bac820ea521d193885a7dd5ff3f62de61826
AAllen10 <168362187+AAllen10@users.noreply.github.com>
2025-02-04 04:07:50 +0000

    Finish exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const alphaNumeric = "abcdefghijklmnopqrstuvwxyz0123456789";
+    const newStr = str
+    .toLowerCase()
+    .split('')
+    .filter((character) => alphaNumeric.includes(character))
+    .join('');
+    const reversedString = newStr.split('').reverse().join('');
+    return reversedString === newStr;
+
+    //originally tried to do via Array.from(str) + regex, overthought
+
 
 };
 
